c2aa0cce8df2f306e62f5eda6164d4cdbc5e11ae
DarkedgeTH <darkedgeth@gmail.com>
2022-02-08 14:05:06 -0500

    added click event

--- src/index.js
+++ src/index.js
@@ -2,20 +2,34 @@ import './less/index.less'
 
 // click event
 
+function signUp() {
+    alert("Congratulations! You're signed up!");
+}
+
+document.querySelectorAll('.btn').forEach(button => button.addEventListener('click', signUp));
+
 // dblclick event
+
 
 // mouseover event
 
+
 // keydown event
+
 
 // wheel event
 
+
 // load event
+
 
 // focus event
 
+
 // resize event
 
+
 // scroll event
+
 
 // select event
