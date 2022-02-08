b796149bcbdef7ae184afce9df78a3d3c3138344
DarkedgeTH <darkedgeth@gmail.com>
2022-02-08 13:45:22 -0500

    set up project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 import './less/index.less'
 
-// Your code goes here!
+// click event
+
+// dblclick event
+
+// mouseover event
+
+// keydown event
+
+// wheel event
+
+// load event
+
+// focus event
+
+// resize event
+
+// scroll event
+
+// select event
